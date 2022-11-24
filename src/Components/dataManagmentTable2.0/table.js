eda0607038e26568bcc66c70b4d147d621814c99
AbdulSami10 <sami.sayani10@gmail.com>
2022-11-24 19:48:18 +0500

    added

--- src/Components/dataManagmentTable2.0/table.js
+++ src/Components/dataManagmentTable2.0/table.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { newData } from "../OldDataManagTable/newData";
+import { newData } from "./newData";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons/lib/icons";
 import { curUser } from "./curUserData";
 import Form from "./Form";
 import CurrentUserUI from "./CurrentUserUI";
-import SearchBar from "./SeacrhBar";
-import { List } from "antd";
+import { Input } from "antd";
+import "./Table.css";
+
+const { Search } = Input;
 
 const getLocalData = () => {
   let data = localStorage.getItem("data");
@@ -22,6 +24,7 @@ function Table() {
   const [rowForm, setRowForm] = useState("");
   const [form, setForm] = useState(false);
   const [editChangeEnable, setEditChangeEnable] = useState(false);
+  const [query, setQuery] = useState("");
 
   const deleteItemHandler = (userName) => {
     setData((prevList) => {
@@ -46,7 +49,15 @@ function Table() {
 
       <span className="tableDiv">
         <span className="tableDiv-top">
-          <SearchBar />
+          <Search
+            placeholder="Search"
+            enterButton
+            className="search"
+            onChange={(e) => {
+              setQuery(e.target.value);
+              console.log(query);
+            }}
+          />
 
           {curUser.userAccess === "Staff" ||
           curUser.userAccess === "Manager" ||
@@ -71,65 +82,71 @@ function Table() {
             <th>Account Status</th>
           </tr>
 
-          {data.map((data) => {
-            return (
-              <tr>
-                <th>
-                  {data.userName}
-                  {curUser.userName === data.userName ? "(You)" : ""}
-                </th>
-                <th>{data.email} </th>
-                <th
-                  className={
-                    data.userAccess.toLowerCase() === "admin"
-                      ? "adminColor"
-                      : data.userAccess.toLowerCase() === "staff"
-                      ? "staffColor"
-                      : data.userAccess.toLowerCase() === "manager"
-                      ? "managerColor"
-                      : data.userAccess.toLowerCase() === "super admin"
-                      ? "superAdminColor"
-                      : ""
-                  }
-                >
-                  {data.userAccess}
-                </th>
-
-                <th>{data.accStatus ? 1 : 0} </th>
-                {curUser.userAccess === "Staff" ||
-                curUser.userAccess === "Restricted" ? (
-                  ""
-                ) : (
+          {data
+            .filter(
+              (user) =>
+                user.userName.toLowerCase().includes(query) ||
+                user.email.toLowerCase().includes(query)
+            )
+            .map((data) => {
+              return (
+                <tr>
+                  <th>
+                    {data.userName}
+                    {curUser.userName === data.userName ? "(You)" : ""}
+                  </th>
+                  <th>{data.email} </th>
                   <th
                     className={
-                      (curUser.userAccess === "Admin" &&
-                        data.userAccess === "Super Admin") ||
-                      (curUser.userAccess === "Manager" &&
-                        data.userAccess === "Super Admin") ||
-                      (curUser.userAccess === "Manager" &&
-                        data.userAccess === "Admin")
-                        ? "restricted"
+                      data.userAccess.toLowerCase() === "admin"
+                        ? "adminColor"
+                        : data.userAccess.toLowerCase() === "staff"
+                        ? "staffColor"
+                        : data.userAccess.toLowerCase() === "manager"
+                        ? "managerColor"
+                        : data.userAccess.toLowerCase() === "super admin"
+                        ? "superAdminColor"
                         : ""
                     }
                   >
-                    <EditOutlined
-                      className="edit"
-                      onClick={() => {
-                        editItem(data.userName);
-                        setEditChangeEnable(true);
-                        setForm(true);
-                      }}
-                    />
-
-                    <DeleteOutlined
-                      className="btn delete"
-                      onClick={() => deleteItemHandler(data.userName)}
-                    />
+                    {data.userAccess}
                   </th>
-                )}
-              </tr>
-            );
-          })}
+
+                  <th>{data.accStatus ? 1 : 0} </th>
+                  {curUser.userAccess === "Staff" ||
+                  curUser.userAccess === "Restricted" ? (
+                    ""
+                  ) : (
+                    <th
+                      className={
+                        (curUser.userAccess === "Admin" &&
+                          data.userAccess === "Super Admin") ||
+                        (curUser.userAccess === "Manager" &&
+                          data.userAccess === "Super Admin") ||
+                        (curUser.userAccess === "Manager" &&
+                          data.userAccess === "Admin")
+                          ? "restricted"
+                          : ""
+                      }
+                    >
+                      <EditOutlined
+                        className="edit"
+                        onClick={() => {
+                          editItem(data.userName);
+                          setEditChangeEnable(true);
+                          setForm(true);
+                        }}
+                      />
+
+                      <DeleteOutlined
+                        className="btn delete"
+                        onClick={() => deleteItemHandler(data.userName)}
+                      />
+                    </th>
+                  )}
+                </tr>
+              );
+            })}
         </table>
       </span>
       {form && (
